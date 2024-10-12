942abc34530254831eb8b05cb2eb1b6c1030f43c
Anshul <choudharyanshul220@gmail.com>
2024-10-12 21:29:34 +0530

    printing in js

---
+++ javascript/js Foundation/printing.js
@@ -0,0 +1,4 @@
+// There are many methods in to print the javascript  output  one and the msot commonly used is console.log(). The another method is process.stdout.write() it does not use in now days, we can use another  console-methods  like  Ctable, Cwarn 
+ 
+console.log("Hello") // always print in new line
+process.stdout.write("Chai") // always print in the single line
