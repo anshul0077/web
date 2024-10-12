3a75e9003b513a05a2427f30bc2a8d376d854434
Anshul <choudharyanshul220@gmail.com>
2024-10-12 22:13:08 +0530

    datatypes

---
+++ javascript/js Foundation/datatypes.js
@@ -0,0 +1,40 @@
+ // 1.   comments in js
+ // i) this is single line comment ,comment's make code relaible  and very helfull to the programmmer . 
+ // ii) /* This is used to make comment block here we can Write any think bewteen in this special characters  */
+
+   
+
+ // Datatypes in the js
+ /*
+ 
+ 1 String
+ 2 Object
+ 3 boolean
+ 4 symbol
+ 5 number
+ 7 null
+ 8 undefined
+ 9 Bigint
+
+ */
+
+
+ // To store any thing in the js we can use the variables 
+
+// We can create variable  with 
+
+/*
+ Let 
+ const
+ Var
+*/
+
+// var  variable-name = value 
+   var score= 100
+                    // this is rearly used or not use in now day's every programmer use the let and const 
+// same as in the case of let and const 
+ let score2=200
+ const score3=400
+
+
+ //example of datatypes
