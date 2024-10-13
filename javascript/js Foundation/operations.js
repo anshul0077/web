55b766b78f081b358e6135aded98bbd1a622457c
Anshul <choudharyanshul220@gmail.com>
2024-10-13 21:26:10 +0530

    realtional done

--- javascript/js Foundation/operations.js
+++ javascript/js Foundation/operations.js
@@ -13,5 +13,22 @@ let div=10/5  //2
 let reminder= 10%5  //0
 let exponent =2**4  // 16
 
+//realtional operation
+
+let one = 3==3  //true
+let two = 3<=1 //false
+let three = 4>=2 //true
+let four = 3<1 //false
+let five = 20>3 //true
+let six = 3!=4  //true
+
+
+// srtict check equaltiy  === this will check the datatype as well
+
+let srtict= 2==='2' // false
+let srtict2 = 2===2 //true
+
+
+//logical operator
 
  
