f3d702ab50db3f009ceaef00181d799229333385
Anshul <choudharyanshul220@gmail.com>
2024-10-13 21:22:00 +0530

    Arithematic operation

---
+++ javascript/js Foundation/operations.js
@@ -0,0 +1,17 @@
+
+// Arithematic operations
+
+let score=10
+let bonus=5
+let total=score + bonus // 15
+
+let add= 5+10  //10
+let sub= 10-5  //5
+let multi=2*3  //6
+let div=10/5  //2
+
+let reminder= 10%5  //0
+let exponent =2**4  // 16
+
+
+ 
