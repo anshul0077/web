b4e6956a783339946d1bc68c18d78876ae9d8cd3
Anshul <choudharyanshul220@gmail.com>
2024-10-14 22:05:50 +0530

    Array

---
+++ javascript/js Foundation/Non-primitive.js
@@ -0,0 +1,24 @@
+// Oject
+
+let username={
+    firstname:"Anshul",
+    Email:"choudharyansher34@gmail.com",
+    age:19
+
+}
+  // we can access any key value pair with the help of dot symbol or operator
+console.log(username.firstname);
+console.log(username.age);
+console.log(username.Email);
+
+// Another methods to access keyvalue pair in objects
+
+console.log(username['Email']);
+console.log(username['firstname']);
+console.log(username['age']);
+
+//Array
+
+
+
+
