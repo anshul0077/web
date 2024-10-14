f9646928eafa529e99101468acb07b4c177bb377
Anshul <choudharyanshul220@gmail.com>
2024-10-14 21:37:56 +0530

    Primitive datatype

---
+++ javascript/js Foundation/Prmitive.js
@@ -0,0 +1,39 @@
+// Number
+
+let num1=10
+let bal= new Number(22)
+console.log(typeof num1);
+console.log( num1);
+
+console.log(typeof bal);
+console.log(bal.valueOf());
+
+//boolean
+let one=true
+let anotherone=new Boolean(true)  //not Recommended
+
+// null and undefined
+
+let firstname;
+console.log(firstname); //undefined
+
+// console.log(lastname);  //null
+
+
+//string
+
+let fname="Anshul"
+let lname="choudhary"
+
+console.log(lname + " " + fname); // old and complex approach
+
+console.log( ` hello ${fname} `)  // Template Literal
+
+
+
+
+
+
+
+
+
