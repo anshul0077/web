d330fc7e863891cdd946bbd95607271bf7683d86
Anshul <choudharyanshul220@gmail.com>
2024-10-14 22:25:24 +0530

    conditional done

---
+++ javascript/js Foundation/conditon.js
@@ -0,0 +1,45 @@
+ // 1
+
+ let num1=3
+ let num2=5
+ if(num1>num2){
+    console.log("num1 is greater then num2");
+    
+ }
+ else{
+    console.log("num1 is less then num2");
+
+ }
+ // 2
+ let fname="Anshul"
+ let lname="Choudhary"
+ if(fname===lname){
+    console.log("Both string are Same");
+    
+ } 
+ else{
+    console.log("String are different");
+    
+ }
+ // 3
+
+ let score=66
+ if(typeof(score)=='number'){
+console.log('Yes');
+
+ }
+ else{
+console.log('not');
+
+ }
+ // 4
+ let bool=true
+ console.log(bool);
+ 
+ // 5
+
+ let arr=[]
+ if(arr.length <=0  ){
+    console.log("arrat is empty");
+    
+ }
