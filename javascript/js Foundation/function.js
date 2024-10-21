0c28c83d0bfd2e18692753e72ec8f1fd3badb1ec
Anshul <choudharyanshul220@gmail.com>
2024-10-21 21:50:49 +0530

    higher order fxn

--- javascript/js Foundation/function.js
+++ javascript/js Foundation/function.js
@@ -18,3 +18,22 @@ const calculateTotal=(prize,quantity)=>{
       return totalCost
 }
 console.log(calculateTotal(10,20));
+
+// higher order function
+function makeTea(typeoftea){
+return `maketea:${typeoftea}`
+}
+function processTeaOrder(Tea){
+return Tea('earl gray')
+}
+let order=processTeaOrder(makeTea)
+console.log(order);
+
+function createTeaMaker(){
+    return function tt (teaType){
+        return `making ${teaType}`
+
+    }
+}
+let teaMaker=createTeaMaker()
+console.log(teaMaker("Green tea"));
