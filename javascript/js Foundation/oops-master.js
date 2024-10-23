2cc62605d9c1be4d4524b8dd374a47f7f60b0e7e
Anshul <choudharyanshul220@gmail.com>
2024-10-23 18:04:58 +0530

    oops in js

---
+++ javascript/js Foundation/oops-master.js
@@ -0,0 +1,61 @@
+let car ={
+    make:"toyota",
+    model:"camry",
+    year:2020,
+    start:function(){
+        return `${this.make} car is got started in ${this.year}`
+    }
+}
+console.log(car.start());
+
+function  Person (name,age,pp){
+    this.name=name
+    this.age=age
+ 
+}
+let p1data=new Person("Anshul",19)
+let p2data=new Person("Shubham",19)
+
+console.log(p1data);
+console.log(p2data);
+
+
+function Animal (name){
+    this.name=name
+}
+Animal.prototype.Sound=function(){
+    return `${this.name} make sound`
+}
+
+let dog= new Animal("Dog")
+console.log(dog.Sound());
+
+Object.prototype.Printname=function(){
+    return 'HEllo'
+}
+
+console.log(dog.Printname());
+
+
+// class
+
+class vehical {
+    constructor(name,age){
+        this.name=name
+        this.age=age
+    }
+    start(){
+        return ` you name ${this.name} and your age is ${this.age}`
+    }
+}
+
+
+class cars extends vehical {
+      drive(){
+        return `Welcome ${this.name}`
+      }
+}
+
+let mycar = new cars("ANshul",20)
+console.log(mycar.start());
+ 
