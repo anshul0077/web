592bee3606ab3f35dc473d4a3716141fcc2b97c5
Anshul <choudharyanshul220@gmail.com>
2024-10-27 21:36:38 +0530

    play with proto

--- javascript/javascript Advance/prototype_inheritance.js
+++ javascript/javascript Advance/prototype_inheritance.js
@@ -6,3 +6,13 @@ namelog.prototype.greet=function(){
 }
 let nn= new namelog("Anshul")
 nn.greet()
+
+function game(score){
+    this.score=score
+
+}
+game.prototype.displayScore=function(){
+    console.log(`This is your score${this.score}`)
+}
+let playerOne= new game(99)
+playerOne.displayScore()
