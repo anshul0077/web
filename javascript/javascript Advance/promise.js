0b754ae27f5daa9602bc6b4a56e0a56fb1dab30a
Anshul <choudharyanshul220@gmail.com>
2024-10-27 12:38:16 +0530

    practice promise

--- javascript/javascript Advance/promise.js
+++ javascript/javascript Advance/promise.js
@@ -16,3 +16,45 @@ response.then((data)=>{
 console.error(error);
 
 })
+
+
+let exersice1= new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+             resolve("You complete your promise")
+        },2000)
+})
+exersice1.then((message)=>{
+console.log(message);
+
+})
+
+let onePromise= new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      resolve("first Promise is done")
+    },1000)
+  
+    
+})
+onePromise.then((mms)=>{
+console.log(mms);
+
+})
+
+
+let login=new Promise((resolve,reject)=>{
+    let user=prompt("Enter value ")
+    if(user==Number){
+        resolve("you are Genius")
+    }
+    else{
+        reject("You are the fucking bitch")
+    }
+})
+login.then((mms)=>{
+console.log(mms);
+
+})
+.catch((error)=>{
+console.error(error);
+
+})
