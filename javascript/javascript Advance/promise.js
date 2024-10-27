b30bd40f48c3d4536e430496cd2c86f1904e1967
Anshul <choudharyanshul220@gmail.com>
2024-10-27 21:05:54 +0530

    make fun with promise

--- javascript/javascript Advance/promise.js
+++ javascript/javascript Advance/promise.js
@@ -41,21 +41,40 @@
 // })
 
 
-let login=new Promise((resolve,reject)=>{
-    let user=prompt("Enter value ")
+// let login=new Promise((resolve,reject)=>{
+//     let user=prompt("Enter value ")
  
       
-    if(user.value==="AAA"){
-        resolve("you are Genius [Fullfilled]")
+//     if(user.value==="AAA"){
+//         resolve("you are Genius [Fullfilled]")
+//     }
+//     else{
+//         reject("You are the fucking bitch [Error is here]")
+//     }
+// })
+// login.then((mms)=>{
+// console.log(mms);
+
+// }).catch((error)=>{
+// console.error(error);
+
+// })
+
+
+let location11 =new Promise((resolve,reject)=>{
+    let User = prompt("Enter your location here")
+    if(User=="Parour"){
+        resolve("yes ,your current location is parour")
     }
     else{
-        reject("You are the fucking bitch [Error is here]")
+        reject("why are you giving wrong location")
     }
 })
-login.then((mms)=>{
-console.log(mms);
-
-}).catch((error)=>{
-console.error(error);
-
+location11.then((mss)=>{
+    console.log(mss);
+    
+})
+.catch((err)=>{
+    console.log(err);
+    
 })
