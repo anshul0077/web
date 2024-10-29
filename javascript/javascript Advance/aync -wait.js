9f93b7e0ca75383727401c9da7a42df56595e386
Anshul <choudharyanshul220@gmail.com>
2024-10-29 22:26:11 +0530

    try catch

--- javascript/javascript Advance/aync -wait.js
+++ javascript/javascript Advance/aync -wait.js
@@ -1,13 +1,30 @@
 
-let userData= new Promise((reslove,reject)=>{
-setTimeout(()=>{
-        reslove({name:"Anshul",age:20})
-},2000)
-})
-userData.then((reslove)=>{
-    console.log(reslove);
-    
-})
+function fetchUserData(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+             reject({name:"Anshul",age:20})
+        },2000)
+
+        // reject("Mishappening in Fetching user data")
+    })
+}
+ let data=fetchUserData()
+// data                           OLD STUFF
+// .then()
+// .catch()
+
+
 async function userData1(){
+try {
+    console.log("fetch data successfully");
+    let data1= await fetchUserData()
+    console.log(data1);
+    
+    
+} catch (error) {
+   
+    console.log(error);
     
 }
+}
+userData1()
