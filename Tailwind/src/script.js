7c8f400b5610cc73900385727923168cfdefb199
Anshul <choudharyanshul220@gmail.com>
2024-11-01 22:53:03 +0530

    stuck to create popup

--- Tailwind/src/script.js
+++ Tailwind/src/script.js
@@ -17,7 +17,7 @@ let recipeDataContainer=document.querySelector(".recipe-data")
         console.log(e.strMealThumb);
        console.log(e.strInstructions);
         
-       clutter+=`<div class="cards h-[75%] text-white  mb-2 flex  items-center justify-around p-4 flex-col shadow-xl rounded-xl w-[32%] bg-green-800">
+       clutter+=`<div  class="-z-10 cards h-[75%] text-white  mb-2 flex  items-center justify-around p-4 flex-col shadow-xl rounded-xl w-[32%] bg-green-800">
             <div class="recipe-img h-3/5  flex items-center justify-center rounded-none w-full ">
 <img class="rounded-2xl h-full" src="${e.strMealThumb}" alt="">
             </div>
@@ -26,6 +26,7 @@ let recipeDataContainer=document.querySelector(".recipe-data")
                 <p class="text-lg">${e.strArea} Dish</p>
                 <p>Belongs to <span class="text-teal-400 text-lg font-mono font-bold">${e.strCategory}</span> category</p>
             </div>
+       <div class="h-96 rounded-3xl w-[700px] hidden bg-red-900 fixed top-52 left-96 z-10"></div>
             <div class="view-recipe">
                 <button class="recipe-button shadow-lg bg-green-500 p-3 rounded-lg pl-6 pr-6">View Recipe</button>
             </div>
@@ -48,3 +49,15 @@ function dishCount(){
     countContainer.innerHTML=` <div class="dish-count text-black font-mono text-xl">Total Dishes <span class="text-teal-600 font-bold">${itemCount}</span></div>`
 
 }
+
+
+recipeDataContainer.addEventListener('click',(e)=>{
+    if(e.target.tagName==="BUTTON"){
+      
+    }
+    else{
+        console.log("nopie");
+        
+    }
+
+})
