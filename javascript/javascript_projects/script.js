40640e1747b27eca7532dcef265c5d8176829fb6
Anshul <choudharyanshul220@gmail.com>
2024-11-04 21:55:36 +0530

    local storage

--- javascript/javascript_projects/script.js
+++ javascript/javascript_projects/script.js
@@ -1,23 +1,31 @@
-let todoInput= document.querySelector("#todo-input")
-let taskBtn= document.querySelector("#add-task-btn")
-let todoList= document.querySelector("#todo-list")
-
-taskBtn.addEventListener('click',()=>{
- let newTask=[]
- let task=todoInput.value.trim()
- if(task==="") return;
- let allTask={
-  id:Date.now(),
-  tasks:task,
-  completed:false,
- }
- newTask.push(allTask)
- todoInput.value=""
- console.log(newTask);
- appendchild(task)
+document.addEventListener('DOMContentLoaded',()=>{
+  let todoInput= document.querySelector("#todo-input")
+  let taskBtn= document.querySelector("#add-task-btn")
+  let todoList= document.querySelector("#todo-list")
+  let newTask= JSON.parse( localStorage.getItem("tasks")) || []
+  newTask.forEach(element=> renderTask(element));
+  taskBtn.addEventListener('click',()=>{
+   let task=todoInput.value.trim()
+   if(task==="") return;
+   let allTask={
+    id:Date.now(),
+    tasks:task,
+    completed:false,
+   }
+   newTask.push(allTask)
+   saveTask()
+   todoInput.value=""
+   renderTask()
+   console.log(newTask);
+  
+  })
+  function saveTask(){
+    localStorage.setItem("tasks",JSON.stringify(newTask))
+  }
+  function renderTask(element){
+    console.log(element);
+    
+  }
+  
+  
 })
-function appendchild(task){
-let li=document.createElement('li')
-li.innerHTML=`${task} <button>Delete</button>`
-todoList.append(li)
-}
