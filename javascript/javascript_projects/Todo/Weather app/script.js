ab08828458097fa8a376c8015e7ac4533cd2fe09
Anshul <choudharyanshul220@gmail.com>
2024-11-14 19:28:48 +0530

    handle api

--- javascript/javascript_projects/Todo/Weather app/script.js
+++ javascript/javascript_projects/Todo/Weather app/script.js
@@ -1,38 +1,66 @@
-document.addEventListener('click',()=>{
-    let cityInput=document.querySelector("#city-input").value.trim()
-    let getWeatherBtn= document.querySelector('#get-weather-btn')
-    let  weatherInfo= document.querySelector("#weather-info")
-    let  cityNameDisplay= document.querySelector("#city-name")
-    let  temperatureDisplay= document.querySelector("#temperature")
-    let  descriptionDisplay= document.querySelector("#description")
-    let  errorMessage= document.querySelector("#error-messag")
+document.addEventListener('click', () => {
+    let cityInput = document.querySelector("#city-input").value.trim()
+    let getWeatherBtn = document.querySelector('#get-weather-btn')
+    let weatherInfo = document.querySelector("#weather-info")
+    let cityNameDisplay = document.querySelector("#city-name")
+    let temperatureDisplay = document.querySelector("#temperature")
+    let descriptionDisplay = document.querySelector("#description")
+    let errorMessage = document.querySelector("#error-message")
 
-    let apiKey="79f003a2f166273ab173f7e0ba705932"
-      getWeatherBtn.addEventListener('click',async()=>{
-       if  (!cityInput) return
-       
-          // server/Database in another continent
+    let apiKey = "79f003a2f166273ab173f7e0ba705932"
+    getWeatherBtn.addEventListener('click', async () => {
+        if (!cityInput) return
 
-          try {
-               const weatherData  =  await fetchWeatherData(cityInput)
-                displayWeatherdata(weatherData)
-          } catch (error) {
-               showError()
-          }
+        // server/Database in another continent
+
+        try {
+            const weatherData = await fetchWeatherData(cityInput,apiKey)
+            displayWeatherdata(weatherData)
+        } catch (error) {
+            showError()
+        }
 
 
-      })
-     async function fetchWeatherData(cityInput,apiKey){
+    })
+    async function fetchWeatherData(cityInput,apiKey) {
+
+        const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}`)
         
+        if (!data.ok) {
+            showError()
+            
+      
+
+        }
+        const respones = await data.json()
+        return respones
 
 
-      }
-      function displayWeatherdata(){
+    }
+    function displayWeatherdata(data) {
+        console.log(data);
+        
+let name=data.name
+ let weather=data.weather[0].main
+ let weatherSpeed=data.wind.speed
+ cityNameDisplay.innerHTML=name
+ temperatureDisplay.innerHTML=weatherSpeed
+ descriptionDisplay.innerHTML=weather
+ console.log(name,weather,weatherSpeed);
+ 
+ weatherInfo.classList.remove("hidden")
+ errorMessage.classList.add("hidden")
 
-      }
-      function showError(){
+ 
+ 
+ 
 
-      }
-
+    }
+    function showError() {
+           weatherInfo.classList.add("hidden")
+          errorMessage.classList.remove("hidden")
     
+    }
+
+
 })
