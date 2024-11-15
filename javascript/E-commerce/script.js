7568f234906e869e8cce5dc9be09b46057e8ad90
Anshul <choudharyanshul220@gmail.com>
2024-11-15 15:22:11 +0530

    assignment

--- javascript/E-commerce/script.js
+++ javascript/E-commerce/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded',()=>{
+
   const product=[
     {id:1,ProductName:"Product 1",Prize:59.99},
     {id:2,ProductName:"Product 2",Prize:29.99},
@@ -11,7 +11,10 @@ let emptyCart=document.querySelector("#empty-cart")
 let CartTOtal=document.querySelector("#cart-total")
 let totalPrize=document.querySelector("#total-price")
 let checkButton=document.querySelector("#checkout-btn")
-  let cart=[]
+  let cart=[] 
+   
+
+
 loadProduct()
   function loadProduct(){
 product.forEach((e)=>{
@@ -38,19 +41,20 @@ product.forEach((e)=>{
   }
 
   function loadCart(prodDets){
+    localStorage.setItem("products",JSON.stringify(prodDets))
               cart.push(prodDets)
        console.log(cart);
    displayCart() 
    
   }
    
+  let total=0
   function displayCart(){
-   let total=0
     cart.forEach((e)=>{
       const cartDiv=document.createElement('div')
       total+=e.Prize
       cartDiv.innerHTML=`
-      <span>${e.ProductName}-$${e.Prize }</span>
+      <span>${e.ProductName}-$${e.Prize } <button style='margin-left:40px; background-color:red ;border-radius:5px;padding:5px;border:none;color:white' data-id=${e.id} >Remove</button> </span>
       `
       totalPrize.textContent=total.toFixed(2)
       if(cart.length>0){
@@ -71,5 +75,28 @@ product.forEach((e)=>{
       
     
   })
+ 
+ 
+  removeItems()
+  function removeItems(){
+    cartItems.addEventListener('click',(e)=>{
+      if (e.target.tagName==="BUTTON"){
+           const removeId= parseInt(  e.target.getAttribute("data-id"))
+         const  getRemoveItem = cart.find(items => items.id===removeId)
+         e.target.parentElement.remove()
   
-})
+       
+         let maindecvalue= parseInt( getRemoveItem.Prize.toFixed(2))
+         
+              total-=maindecvalue
+   
+      console.log(total);
+      console.log(maindecvalue);
+  
+     
+      
+      totalPrize.innerHTML=total.toFixed(2)
+      }
+    })
+  }
+  
