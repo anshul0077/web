93e278a988df3ec2db6bc3aa72d9572f82c366a8
Anshul <choudharyanshul220@gmail.com>
2024-11-17 20:23:08 +0530

    expense tracker complete

--- javascript/Expense Tracker/script.js
+++ javascript/Expense Tracker/script.js
@@ -4,15 +4,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const expenseAmountInput = document.getElementById("expense-amount");
   const expenseList = document.getElementById("expense-list");
   const totalAmountDisplay = document.getElementById("total-amount");
-let expense=JSON.parse(localStorage.getItem("EXPENSE")) ||[]
+  let expense=JSON.parse(localStorage.getItem("EXPENSE")) ||[]
+  maintotal()
 renderList()
-let totalAmount=calculate()
-console.log(totalAmount);
+function maintotal(){
+  let totalmain=(JSON.parse(localStorage.getItem("Total")))
+  totalAmountDisplay.textContent=totalmain
 
+}
 expenseForm.addEventListener('click',(e)=>{
-e.preventDefault()
-const name=expenseNameInput.value.trim()
-const amount=parseInt(expenseAmountInput.value.trim())
+  e.preventDefault()
+  const name=expenseNameInput.value.trim()
+  const amount=parseInt(expenseAmountInput.value.trim())
 if(name !=="" && !isNaN(amount) && amount>0){
   const newExpense={
     id:Date.now(),
@@ -20,9 +23,10 @@ if(name !=="" && !isNaN(amount) && amount>0){
     amount:amount,
   }
   expense.push(newExpense)
-  saveToLocal()
   renderList()
- updateTotal()
+  updateTotal()
+  saveToLocal()
+  maintotal()
   //clear input
   expenseNameInput.value=""
   expenseAmountInput.value=""
@@ -31,6 +35,7 @@ if(name !=="" && !isNaN(amount) && amount>0){
 
 }) 
 function renderList(){
+  expenseList.innerHTML=""
   expense.forEach((e)=>{
     let li=document.createElement('li')
     li.innerHTML=` <span>${e.name}--$${e.amount}</span>  <button data-id=${e.id} >Delete</button>`
@@ -41,21 +46,24 @@ function renderList(){
 
 }
 
+
 expenseList.addEventListener('click',(e)=>{
   if(e.target.tagName==="BUTTON"){
     e.target.parentElement.remove()
    exId= parseInt(e.target.getAttribute('data-id'))
     expense=expense.filter((items)=>items.id !== exId)
-    saveToLocal()
     updateTotal()
+    saveToLocal()
+    maintotal()
     renderList()
   }
   
 })
+let totalAmount
 function updateTotal(){
   totalAmount=calculate()
-  totalAmountDisplay.textContent=totalAmount.toFixed(2)
 }
+
 
 function calculate(){
   let total=0
@@ -63,5 +71,7 @@ return expense.reduce((acc,expense)=>acc+expense.amount,total)
 }
 function saveToLocal(){
   localStorage.setItem("EXPENSE",JSON.stringify(expense))
+  localStorage.setItem("Total",JSON.stringify(totalAmount))
 }
+
 });
